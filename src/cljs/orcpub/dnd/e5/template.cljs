ab63983154a73a13355c8e8ce9d78aae6681458f
Larry <larry.d.christensen@gmail.com>
2017-03-09 12:40:52 -0700

    fix warlock traits

--- src/cljs/orcpub/dnd/e5/template.cljs
+++ src/cljs/orcpub/dnd/e5/template.cljs
@@ -2568,65 +2568,24 @@ You can't manifest your wings while wearing armor unless the armor is made to ac
     :pact-of-the-chain
     []
     [(mod5e/trait pact-of-the-chain-name
-                  "You learn the find familiar spell and can cast it as a 
-ritual. The spell doesn’t count against your number 
-of spells known.
-When you cast the spell, you can choose one of the 
-normal forms for your familiar or one of the 
-following special forms: imp, pseudodragon, quasit, 
-or sprite.
-Additionally, when you take the Attack action, you 
-can forgo one of your own attacks to allow your 
-familiar to make one attack of its own with its 
-reaction.")])
+                  "You learn the find familiar spell and can cast it as a ritual. The spell doesn’t count against your number of spells known.
+When you cast the spell, you can choose one of the normal forms for your familiar or one of the following special forms: imp, pseudodragon, quasit, or sprite.
+Additionally, when you take the Attack action, you can forgo one of your own attacks to allow your familiar to make one attack of its own with its reaction.")])
    (t/option
     "Pact of the Blade"
     :pact-of-the-blade
     []
     [(mod5e/trait pact-of-the-blade-name
-                  "You can use your action to create a pact weapon in 
-your empty hand. You can choose the form that this 
-melee weapon takes each time you create it. You are 
-proficient with it while you wield it. This weapon 
-counts as magical for the purpose of overcoming 
-resistance and immunity to nonmagical attacks and 
-damage.
-Your pact weapon disappears if it is more than 5 
-feet away from you for 1 minute or more. It also 
-disappears if you use this feature again, if you 
-dismiss the weapon (no action required), or if you 
-die.
-You can transform one magic weapon into your 
-pact weapon by performing a special ritual while 
-you hold the weapon. You perform the ritual over 
-the course of 1 hour, which can be done during a 
-short rest. You can then dismiss the weapon, 
-shunting it into an extradimensional space, and it 
-appears whenever you create your pact weapon 
-thereafter. You can’t affect an artifact or a sentient 
-weapon in this way. The weapon ceases being your pact weapon if you die, if you perform the 1-hour 
-ritual on a different weapon, or if you use a 1-hour 
-ritual to break your bond to it. The weapon appears 
-at your feet if it is in the extradimensional space 
-when the bond breaks.")])
+                  "You can use your action to create a pact weapon in your empty hand. You can choose the form that this melee weapon takes each time you create it. You are proficient with it while you wield it. This weapon counts as magical for the purpose of overcoming resistance and immunity to nonmagical attacks and damage.
+Your pact weapon disappears if it is more than 5 feet away from you for 1 minute or more. It also disappears if you use this feature again, if you dismiss the weapon (no action required), or if you die.
+You can transform one magic weapon into your pact weapon by performing a special ritual while you hold the weapon. You perform the ritual over the course of 1 hour, which can be done during a short rest. You can then dismiss the weapon, hunting it into an extradimensional space, and it appears whenever you create your pact weapon thereafter. You can’t affect an artifact or a sentient weapon in this way. The weapon ceases being your pact weapon if you die, if you perform the 1-hour ritual on a different weapon, or if you use a 1-hour ritual to break your bond to it. The weapon appears at your feet if it is in the extradimensional space when the bond breaks.")])
    (t/option
     "Pact of the Tome"
     :pact-of-the-tome
     []
     [(mod5e/trait pact-of-the-tome-name
-                  "Your patron gives you a grimoire called a Book of 
-Shadows. When you gain this feature, choose three 
-cantrips from any class’s spell list (the three needn’t 
-be from the same list). While the book is on your 
-person, you can cast those cantrips at will. They 
-don’t count against your number of cantrips known. 
-If they don’t appear on the warlock spell list, they 
-are nonetheless warlock spells for you.
-If you lose your Book of Shadows, you can perform 
-a 1-hour ceremony to receive a replacement from 
-your patron. This ceremony can be performed 
-during a short or long rest, and it destroys the 
-previous book. The book turns to ash when you die.")])])
+                  "Your patron gives you a grimoire called a Book of Shadows. When you gain this feature, choose three cantrips from any class’s spell list (the three needn’t be from the same list). While the book is on your person, you can cast those cantrips at will. They don’t count against your number of cantrips known. If they don’t appear on the warlock spell list, they are nonetheless warlock spells for you.
+If you lose your Book of Shadows, you can perform a 1-hour ceremony to receive a replacement from your patron. This ceremony can be performed during a short or long rest, and it destroys the previous book. The book turns to ash when you die.")])])
 
 
 (defn wizard-option [character-ref]
