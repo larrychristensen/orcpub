f2536196831f3ce358ccc80e85b2820ef761f312
Larry <larry.d.christensen@gmail.com>
2017-05-29 09:12:53 -0600

    fix character round trip

--- test/cljc/orcpub/common_test.clj
+++ test/cljc/orcpub/common_test.clj
@@ -5,7 +5,10 @@
 
 (deftest test-add-namespaces-to-keys
   (stest/instrument `common/add-namespaces-to-keys)
-  (is (thrown? Exception (common/add-namespaces-to-keys "a.b.c" {::x 1})))
-  (is (= {:a.b.c/x 1
-          :a.b.c/y "sdlk"} (common/add-namespaces-to-keys "a.b.c" {:x 1 :y "sdlk"})))
+  (is (= {:db/id 88
+          :a.b.c/x 1
+          :a.b.c/y "sdlk"}
+         (common/add-namespaces-to-keys "a.b.c" {:db/id 88
+                                                 :x 1
+                                                 :y "sdlk"})))
   (stest/instrument `common/add-namespaces-to-keys))
--- test/cljc/orcpub/dnd/e5/character_test.clj
+++ test/cljc/orcpub/dnd/e5/character_test.clj
@@ -1,6 +1,7 @@
 (ns orcpub.dnd.e5.character-test
   (:require [clojure.test :refer [is deftest testing]]
             [clojure.spec :as spec]
+            [clojure.data :refer [diff]]
             [orcpub.dnd.e5.character :as char]
             [orcpub.dnd.e5.character.equipment :as equip]
             [orcpub.entity :as entity]))
@@ -95,3 +96,10 @@
   (let [character {:orcpub.entity/options {:weapons [{:orcpub.entity/key :javelin, :orcpub.entity/value {:orcpub.dnd.e5.character.equipment/quantity 4, :orcpub.dnd.e5.character.equipment/equipped? true, :orcpub.dnd.e5.character.equipment/class-starting-equipment? true}} {:orcpub.entity/key :handaxe, :orcpub.entity/value {:orcpub.dnd.e5.character.equipment/equipped? true, :orcpub.dnd.e5.character.equipment/quantity "2"}} {:orcpub.entity/key :greataxe, :orcpub.entity/value {:orcpub.dnd.e5.character.equipment/quantity 1, :orcpub.dnd.e5.character.equipment/equipped? true}}]}}
         expected {:orcpub.entity/options {:weapons [{:orcpub.entity/key :javelin, :orcpub.entity/value {:orcpub.dnd.e5.character.equipment/quantity 4, :orcpub.dnd.e5.character.equipment/equipped? true, :orcpub.dnd.e5.character.equipment/class-starting-equipment? true}} {:orcpub.entity/key :handaxe, :orcpub.entity/value {:orcpub.dnd.e5.character.equipment/equipped? true, :orcpub.dnd.e5.character.equipment/quantity 2}} {:orcpub.entity/key :greataxe, :orcpub.entity/value {:orcpub.dnd.e5.character.equipment/quantity 1, :orcpub.dnd.e5.character.equipment/equipped? true}}]}}]
     (is (= (char/fix-quantities character) expected))))
+
+
+(deftest strict-round-trip
+  (let [strict {:db/id 17592186056152, :orcpub.entity.strict/selections [{:db/id 17592186056153, :orcpub.entity.strict/key :ability-scores, :orcpub.entity.strict/option {:db/id 17592186056154, :orcpub.entity.strict/key :standard-scores, :orcpub.entity.strict/map-value {:db/id 17592186056155, :orcpub.dnd.e5.character/str 15, :orcpub.dnd.e5.character/dex 14, :orcpub.dnd.e5.character/con 13, :orcpub.dnd.e5.character/int 12, :orcpub.dnd.e5.character/wis 10, :orcpub.dnd.e5.character/cha 8}}} {:db/id 17592186056156, :orcpub.entity.strict/key :class, :orcpub.entity.strict/options [{:db/id 17592186056157, :orcpub.entity.strict/key :barbarian, :orcpub.entity.strict/selections [{:db/id 17592186056158, :orcpub.entity.strict/key :levels, :orcpub.entity.strict/options [{:db/id 17592186056159, :orcpub.entity.strict/key :level-1}]}]}]} {:db/id 17592186056160, :orcpub.entity.strict/key :weapons, :orcpub.entity.strict/options [{:db/id 17592186056161, :orcpub.entity.strict/key :javelin, :orcpub.entity.strict/map-value {:db/id 17592186056162, :orcpub.dnd.e5.character.equipment/quantity 4, :orcpub.dnd.e5.character.equipment/equipped? true, :orcpub.dnd.e5.character.equipment/class-starting-equipment? true}}]} {:db/id 17592186056163, :orcpub.entity.strict/key :equipment, :orcpub.entity.strict/options [{:db/id 17592186056164, :orcpub.entity.strict/key :explorers-pack, :orcpub.entity.strict/map-value {:db/id 17592186056165, :orcpub.dnd.e5.character.equipment/quantity 1, :orcpub.dnd.e5.character.equipment/equipped? true, :orcpub.dnd.e5.character.equipment/class-starting-equipment? true}}]}]}
+        round-trip (-> strict char/from-strict char/to-strict)]
+    (prn "DIFF" (diff strict round-trip))
+    (is (= strict round-trip))))
