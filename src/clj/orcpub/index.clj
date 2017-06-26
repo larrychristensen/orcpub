e12bfa0c0c507bec688475561d6e718adc0f54c2
Larry <larry.d.christensen@gmail.com>
2017-06-26 16:20:13 -0600

    fix script in index.clj

--- src/clj/orcpub/index.clj
+++ src/clj/orcpub/index.clj
@@ -25,23 +25,7 @@
             :content "width=device-width, initial-scale=1"}]
     [:title title]
     (include-css "/css/style.css"
-                 "/css/compiled/styles.css")]
-   [:body {:style "margin:0"}
-    [:div#app
-     [:div {:style "display:flex;justify-content:space-around"}
-      [:img {:src "/image/spiral.gif"
-             :style "height:200px;width:200px;margin-top:200px"}]]]
-    (include-js "/js/compiled/orcpub.js")
-    (include-css "/font-awesome-4.7.0/css/font-awesome.min.css")
-    (include-css "https://fonts.googleapis.com/css?family=Open+Sans")
-    [:script
-     "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-	  ga('create', 'UA-69209720-3', 'auto');
-	  ga('send', 'pageview');"]
+                 "/css/compiled/styles.css")
     [:script
      "window.fbAsyncInit = function() {
 	  FB.init({
@@ -58,4 +42,20 @@
 	  js = d.createElement(s); js.id = id;
 	  js.src = \"//connect.facebook.net/en_US/sdk.js\";
 	  fjs.parentNode.insertBefore(js, fjs);
-	  }(document, 'script', 'facebook-jssdk'));"]]))
+	  }(document, 'script', 'facebook-jssdk'));"]]
+   [:body {:style "margin:0"}
+    [:div#app
+     [:div {:style "display:flex;justify-content:space-around"}
+      [:img {:src "/image/spiral.gif"
+             :style "height:200px;width:200px;margin-top:200px"}]]]
+    (include-js "/js/compiled/orcpub.js")
+    (include-css "/font-awesome-4.7.0/css/font-awesome.min.css")
+    (include-css "https://fonts.googleapis.com/css?family=Open+Sans")
+    [:script
+     "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-69209720-3', 'auto');
+	  ga('send', 'pageview');"]]))
