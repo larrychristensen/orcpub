c5e096b558620e67fcedd2a02badb4e6376b5155
Larry <larry.d.christensen@gmail.com>
2017-07-27 14:55:28 -0600

    dual build mostly working; basic options selectors working

--- env/dev/env/index.cljs
+++ env/dev/env/index.cljs
@@ -5,6 +5,6 @@
 (set! js/window.goog js/undefined)
 
 (-> (js/require "figwheel-bridge")
-    (.withModules #js {"react-native" (js/require "react-native"), "./assets/images/orcpub-logo.png" (js/require "../../assets/images/orcpub-logo.png"), "react" (js/require "react"), "expo" (js/require "expo"), "./assets/icons/app.png" (js/require "../../assets/icons/app.png"), "./assets/icons/loading.png" (js/require "../../assets/icons/loading.png"), "./assets/images/cljs.png" (js/require "../../assets/images/cljs.png"), "./assets/images/orcpub-logo.svg" (js/require "../../assets/images/orcpub-logo.svg")}
+    (.withModules #js {"react" (js/require "react"), "react-native" (js/require "react-native"), "expo" (js/require "expo"), "./assets/icons/app.png" (js/require "../../assets/icons/app.png"), "./assets/icons/loading.png" (js/require "../../assets/icons/loading.png"), "./assets/images/cljs.png" (js/require "../../assets/images/cljs.png"), "./assets/images/orcpub-logo.svg" (js/require "../../assets/images/orcpub-logo.svg"), "./assets/images/orcpub-logo.png" (js/require "../../assets/images/orcpub-logo.png")}
 )
     (.start "main"))
