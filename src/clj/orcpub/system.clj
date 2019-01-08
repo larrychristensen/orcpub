5b6c6624a2f7b3cfc374ef712692c9bb1f1845ba
Jakub Błaszczyk <jakub.blaszczyk@sap.com>
2019-01-08 19:40:35 +0100

    Implement suggestions

--- src/clj/orcpub/system.clj
+++ src/clj/orcpub/system.clj
@@ -35,8 +35,8 @@
     :conn
     (datomic/new-datomic
       (if-let [datomic-url (:datomic-url environ/env)]
-        ; (str datomic-url "?aws_access_key_id=" (environ/env :datomic-access-key) "&aws_secret_key=" (environ/env :datomic-secret-key))
-        (str datomic-url)
+        (str datomic-url "?aws_access_key_id=" (environ/env :datomic-access-key) "&aws_secret_key=" (environ/env :datomic-secret-key))
+        ; (str datomic-url)
         (when true #_(= :dev env)
           (println "WARN: no :datomic-url environment variable set; using local dev")
           "datomic:free://localhost:4334/orcpub")))
